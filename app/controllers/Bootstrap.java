b9882a88705f6fab78455e7d3e80ad263030d149
harvey <heweimr@gmail.com>
2017-03-29 21:51:09 +0800

    实现MQTT订阅、mongodb存储，下一步可以用morphia实现数据库存取

--- app/controllers/Bootstrap.java
+++ app/controllers/Bootstrap.java
@@ -21,6 +21,20 @@ import edu.pc3.sensoract.vpds.api.request.UserRegisterFormat;
 import edu.pc3.sensoract.vpds.constants.Const;
 import edu.pc3.sensoract.vpds.model.UserProfileModel;
 
+
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;  
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttTopic;  
+
+
+import com.mongodb.*;
+
+
 /**
  * @author samy
  * 
@@ -151,11 +165,108 @@ public class Bootstrap extends Job {
 		SensorActAPI.userProfile.addUserProfile(owner, ownerKey);
 	}
 
+//	启动MQTTsub
+	public class mqttSub {  
+		  
+	    public static final String HOST = "tcp://localhost:1883";  
+	    public static final String TOPIC = "TEST";  
+	    private MqttClient client;  
+	    private MqttConnectOptions options;  
+	    
+	    private void start() {  
+	        try {  
+	            // host为主机名，clientid即连接MQTT的客户端ID，一般以唯一标识符表示，MemoryPersistence设置clientid的保存形式，默认为以内存保存  
+	            client = new MqttClient("tcp://127.0.0.1:1883", "pahomqttPublish1"); 
+	            
+	            // MQTT的连接设置  
+	            options = new MqttConnectOptions();  
+	            
+	            // 设置回调  
+	            client.setCallback(new PushCallback()); 
+	            
+	            MqttTopic topic = client.getTopic(TOPIC);  
+	            //setWill方法，如果项目中需要知道客户端是否掉线可以调用该方法。设置最终端口的通知消息  
+	            options.setWill(topic, "close".getBytes(), 2, true);  
+	  
+	            client.connect(options);  
+	            
+	            //订阅消息  
+	            int[] Qos  = {1};  
+	            String[] topic1 = {TOPIC}; 
+	            
+	        	client.subscribe(topic1, Qos);
+	                
+	        } catch (Exception e) {  
+	            System.out.println("Error");
+	            e.printStackTrace();  
+
+	        }  
+	    }  
+	    
+	    //回调函数    
+	    public class PushCallback implements MqttCallback {  
+	    	  
+	    	public void connectionLost(Throwable cause) {  
+		        // 连接丢失后，一般在这里面进行重连  
+		        System.out.println("连接断开，可以做重连");  
+		    }    
+		    public void deliveryComplete(IMqttDeliveryToken token) {  
+		        System.out.println("deliveryComplete---------" + token.isComplete());  
+		    }  
+		  
+		    public void messageArrived(String topic, MqttMessage message) throws Exception {  
+		        // subscribe后得到的消息会执行到这里面  
+		        System.out.println("接收消息主题 : " + topic);  
+		        System.out.println("接收消息Qos : " + message.getQos());  
+		        System.out.println("接收消息内容 : " + new String(message.getPayload()));  
+		        
+		        
+		        
+		      //数据库
+		        try{	        	
+		        	Mongo mongo = new Mongo("127.0.0.1",27017);  
+		            DB db =mongo.getDB("study"); 
+		            DBCollection users = db.getCollection("person");
+		            
+		            DBObject user = new BasicDBObject();  
+		            user.put("name", "jimmy");  
+		            user.put("age", "34");  
+		            DBObject address = new BasicDBObject();  
+		            address.put("city", "bj");  
+		            address.put("street", "bq road");  
+		            address.put("mail", "ufpark 68#");   
+		            user.put("address", address);  
+	  
+		            users.insert(user);  
+		            
+		            // 从集合中查询数据，我们就查询一条，调用findOne即可  
+		            DBObject dbUser = users.findOne();  
+		            System.out.println("插入数据库：");
+		            System.out.println("name" + " : "  + dbUser.get("name") );  
+		            System.out.println("age" + " : "  + dbUser.get("age") );  
+		            DBObject dbAddress = (DBObject)user.get("address");  
+		            System.out.println("city" + " : "  + dbAddress.get("city") );  
+		            System.out.println("street" + " : "  + dbAddress.get("street") );  
+		            System.out.println("mail" + " : "  + dbAddress.get("mail") );  
+		            
+		        }catch(Exception e){
+		        	System.out.println("DB error"); 
+		        }
+		    }   
+	    } 
+	    
+	}  
+	
 	public void doJob() {
 
 		getOwnerConfiguration();
 		verifyOwnerKeys();
 		addOwnerProfile();
+		
+		mqttSub client = new mqttSub();  
+        client.start();
+        
+        System.out.println("aaaaaaa");
 
 		// Play.configuration.list(System.out);
 
