a109416c94b4f0e10d670070a8340c2538f2610e
harvey <heweimr@gmail.com>
2017-04-27 20:48:38 +0800

    将设备端数据存入数据库mongodb

--- app/javacode/MQTTsub.java
+++ app/javacode/MQTTsub.java
@@ -14,6 +14,19 @@ import edu.pc3.sensoract.vpds.api.SensorActAPI;
 import javacode.*;
 import controllers.Bootstrap;
 
+
+
+//mongodb
+//import java.net.UnknownHostException;
+//import java.util.Set;
+//import com.mongodb.DB;
+//import com.mongodb.DBCollection;
+//import com.mongodb.Mongo;
+//import com.mongodb.MongoException;
+import com.mongodb.*;
+import com.mongodb.util.JSON;
+
+
 //mqtt sub
 	public class MQTTsub {  
 		  
@@ -97,6 +110,39 @@ import controllers.Bootstrap;
 			        SensorActAPI.deviceAdd.doProcess(new String(message.getPayload()));
 		        }
 		        
+		        //mongodb
+		        try{	        	
+		        	Mongo mongo = new Mongo("127.0.0.1",27017);  
+		            DB db =mongo.getDB("study"); 
+		            DBCollection collection = db.getCollection(TOPIC);
+		            String jsonContent = new String(message.getPayload());
+		            DBObject dbObject =(DBObject)JSON.parse(jsonContent);
+		            collection.insert(dbObject);
+		            
+//		            DBObject user = new BasicDBObject();  
+//		            user.put("name", "jimmy");  
+//		            user.put("age", "34");  
+//		            DBObject address = new BasicDBObject();  
+//		            address.put("city", "bj");  
+//		            address.put("street", "bq road");  
+//		            address.put("mail", "ufpark 68#");   
+//		            user.put("address", address);  
+//	  
+//		            users.insert(user);  
+//		            
+		            // 从集合中查询数据，我们就查询一条，调用findOne即可  
+//		            DBObject dbUser = users.findOne();  
+//		            System.out.println("插入数据库：");
+//		            System.out.println("name" + " : "  + dbUser.get("name") );  
+//		            System.out.println("age" + " : "  + dbUser.get("age") );  
+//		            DBObject dbAddress = (DBObject)user.get("address");  
+//		            System.out.println("city" + " : "  + dbAddress.get("city") );  
+//		            System.out.println("street" + " : "  + dbAddress.get("street") );  
+//		            System.out.println("mail" + " : "  + dbAddress.get("mail") );  
+		            
+		        }catch(Exception e){
+		        	System.out.println("DB error"); 
+		        }
 		        
 		        
 		        
